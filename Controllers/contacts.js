38b913bdf74c336e539c2b25bba365a0f312f9a1
Manish Kumar <manishhansalmeh@gmail.com>
2022-08-10 18:56:42 +0530

    Update

--- Controllers/contacts.js
+++ Controllers/contacts.js
@@ -34,7 +34,7 @@ async function getAllMessages(req, res, next) {
 async function createMessages(req, res, next) {
   let date = new Date();
   let year = date.getFullYear();
-  let month = date.getMonth();
+  let month = date.getMonth() + 1;
   let day = date.getDate();
   let hours = date.getHours();
   let minutes = date.getMinutes();
